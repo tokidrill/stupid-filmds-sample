facfd542bcd57642f754e402642dafda849944b7
SASAKIYuki <tokidrill@gmail.com>
2020-02-18 01:41:39 +0900

    Init migration

---
+++ src/phpmig.php
@@ -0,0 +1,16 @@
+<?php
+
+use Phpmig\Adapter;
+
+$container = new ArrayObject();
+
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET CHARACTER SET 'utf8'");
+$dbh = new PDO('mysql:host=db;dbname=filmds', 'dev', 'secret', $options);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// replace this with a better Phpmig\Adapter\AdapterInterface
+$container['phpmig.adapter'] = new Adapter\PDO\Sql($dbh, 'phpmig');
+$container['phpmig.migrations_path'] = __DIR__ . DIRECTORY_SEPARATOR . 'migrations';
+$container['db'] = $dbh;
+
+return $container;
